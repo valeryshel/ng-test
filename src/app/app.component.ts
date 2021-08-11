ea513ad210135b4a8c60e44f25c08980b60b49e5
valeryshel <valeryshel@gmail.com>
2021-08-11 21:33:13 +0300

    init: start todo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  date?: any
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-test';
+  appTitle = 'Angular todo application'
+  // title = 'ng-test';
+  public todos: Todo [] = [
+    { id: 1, title: 'to buy a bread', completed: false, date: new Date()},
+    { id: 2, title: 'to buy an oil', completed: true, date: new Date()},
+    { id: 1, title: 'to buy a beer', completed: false, date: new Date()},
+  ]
 }
